12aa2d4e35b9ff42ffb54a9dd3402528854a2fa5
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-05-26 13:42:58 +0200

    edit to script

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_DWA05/scripts.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_DWA05/scripts.js
@@ -1,9 +1,34 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+
+  const quotient = dividend / divider;
+
+  if (dividend === "" || divider === "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try Again.";   
+  } else if (dividend <= 0 || divider <= 0) {
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+    throw new Error("Invalid number provided. Try again");
+  } else if (isNaN(dividend) || isNaN(divider)) {
+    document.body.innerHTML = "Something critical went wrong. Please reload the page";
+    throw new Error('Something critical went wrong. Please reload the page');
+  } else {
+    if (quotient % 1 > 0) {
+      result.innerText = Math.floor(quotient);
+    } else {
+      result.innerText = quotient;
+    }
+  }
 });
+
+
+/* Alternative error and call stack tracing:
+console.error('Something critical went wrong. Please reload the page');
+    console.trace();
+*/
