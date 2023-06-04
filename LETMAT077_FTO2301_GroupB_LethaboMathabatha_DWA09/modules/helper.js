dd40ba64acc4453f51fd58cc9742be7bdfcc6da0
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-06-04 19:05:51 +0200

    added DWA9 files

---
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_DWA09/modules/helper.js
@@ -0,0 +1,305 @@
+/**
+ * Generates a document fragment containing a list of buttons, each representing a starting book.
+ *
+ * @param {Array} matches - The list of books.
+ * @param {number} BOOKS_PER_PAGE - The number of books to display per page.
+ * @param {object} authors - An object containing the authors of the books.
+ * @return {DocumentFragment} A document fragment containing a list of buttons representing the starting books.
+ */
+export function createBookGenerator() {
+  const getStartingBooks = (matches, BOOKS_PER_PAGE, authors) => {
+    const starting = document.createDocumentFragment();
+
+    for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
+      const element = document.createElement('button');
+      element.classList = 'preview';
+      element.setAttribute('data-preview', id);
+
+      element.innerHTML = `
+          <img
+              class="preview__image"
+              src="${image}"
+          />
+          
+          <div class="preview__info">
+              <h3 class="preview__title">${title}</h3>
+              <div class="preview__author">${authors[author]}</div>
+          </div>
+      `;
+
+      starting.appendChild(element);
+    }
+
+    return starting;
+  };
+
+  return {
+    getStartingBooks,
+  };
+}
+
+
+
+/**
+ * Creates a document fragment of genre options for a select element based on
+ * the received genres object.
+ * @param {Object} genres - An object containing genre id and name pairs.
+ * @return {DocumentFragment} A document fragment containing the genre options.
+ */
+export function createGenreOptionsFragment(options) {
+  const genreHtml = document.createDocumentFragment();
+
+  for (const { value, text } of options) {
+    const element = createGenreOptionsElement(value, text);
+    genreHtml.appendChild(element);
+  }
+
+  return genreHtml;
+}
+
+function createGenreOptionsElement(value, text) {
+  const element = document.createElement("option");
+  element.value = value;
+  element.innerText = text;
+  return element;
+}
+
+
+
+/**
+ * Creates a document fragment of author option elements based on the received options array.
+ * @param {Array} options - An array of option objects.
+ * @return {DocumentFragment} A document fragment containing the option elements.
+ */
+export function createAuthorOptionsFragment(options) {
+  const fragment = document.createDocumentFragment();
+
+  for (const { value, text } of options) {
+    const element = document.createElement("option");
+    element.value = value;
+    element.innerText = text;
+    fragment.appendChild(element);
+  }
+
+  return fragment;
+}
+ 
+
+/**
+ * Sets the theme of the page to either "night" or "day" by changing the background colors
+ * and updating the value of a data attribute.
+ *
+ * @param {string} theme - The theme to set the page to, either "night" or "day".
+ * @return {void} This function does not return anything.
+ */
+export function setTheme(theme) {
+  {
+    const colorDark = theme === "night" ? "255, 255, 255" : "10, 10, 20";
+    const colorLight = theme === "night" ? "10, 10, 20" : "255, 255, 255";
+
+    document.querySelector("[data-settings-theme]").value = theme;
+    document.documentElement.style.setProperty("--color-dark", colorDark);
+    document.documentElement.style.setProperty("--color-light", colorLight);
+  }
+}
+
+
+/**
+ * Handles the settings form submission and updates the theme of the document.
+ *
+ * @param {Event} event - The submit event triggered by the form.
+ * @return {void} This function does not return anything.
+ */
+export function handleSettingsFormSubmit(event) {
+
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const { theme } = Object.fromEntries(formData);
+
+    if (theme === "night") {
+      document.documentElement.style.setProperty(
+        "--color-dark",
+        "255, 255, 255"
+      );
+      document.documentElement.style.setProperty("--color-light", "10, 10, 20");
+    } else {
+      document.documentElement.style.setProperty("--color-dark", "10, 10, 20");
+      document.documentElement.style.setProperty(
+        "--color-light",
+        "255, 255, 255"
+      );
+    }
+
+    document.querySelector("[data-settings-overlay]").open = false;
+  };
+
+
+
+/**
+ * Processes the search form given the authors, books, BOOKS_PER_PAGE, page, and matches.
+ *
+ * @param {Array} authors - an array of author names
+ * @param {Array} books - an array of book objects
+ * @param {number} BOOKS_PER_PAGE - the number of books to display per page
+ * @param {number} page - the current page number
+ * @param {Array} matches - an array of book objects that match the search criteria
+ * @return {void} This function does not return anything.
+ */
+
+
+export function processSearchForm(authors, books, BOOKS_PER_PAGE, page, matches) {
+  document
+  .querySelector("[data-search-form]")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const filters = Object.fromEntries(formData);
+    const result = [];
+
+    for (const book of books) {
+      let genreMatch = filters.genre === "any";
+
+      for (const singleGenre of book.genres) {
+        if (genreMatch) break;
+        if (singleGenre === filters.genre) {
+          genreMatch = true;
+        }
+      }
+
+      if (
+        (filters.title.trim() === "" ||
+          book.title.toLowerCase().includes(filters.title.toLowerCase())) &&
+        (filters.author === "any" || book.author === filters.author) &&
+        genreMatch
+      ) {
+        result.push(book);
+      }
+    }
+
+    page = 1;
+    matches = result;
+
+    if (result.length < 1) {
+      document
+        .querySelector("[data-list-message]")
+        .classList.add("list__message_show");
+    } else {
+      document
+        .querySelector("[data-list-message]")
+        .classList.remove("list__message_show");
+    }
+
+    document.querySelector("[data-list-items]").innerHTML = "";
+    const newItems = document.createDocumentFragment();
+
+    for (const { author, id, image, title } of result.slice(
+      0,
+      BOOKS_PER_PAGE
+    )) {
+      const element = document.createElement("button");
+      element.classList = "preview";
+      element.setAttribute("data-preview", id);
+
+      element.innerHTML = `
+            <img
+                class="preview__image"
+                src="${image}"
+            />
+            
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${authors[author]}</div>
+            </div>
+        `;
+
+      newItems.appendChild(element);
+    }
+
+    document.querySelector("[data-list-items]").appendChild(newItems);
+    document.querySelector("[data-list-button]").disabled =
+      matches.length - page * BOOKS_PER_PAGE < 1;
+
+    document.querySelector("[data-list-button]").innerHTML = `
+        <span>Show more</span>
+        <span class="list__remaining"> (${
+          matches.length - page * BOOKS_PER_PAGE > 0
+            ? matches.length - page * BOOKS_PER_PAGE
+            : 0
+        })</span>
+    `;
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    document.querySelector("[data-search-overlay]").open = false;
+  });
+}
+
+
+export function handlePreviews(authors, books, BOOKS_PER_PAGE, page, matches) {
+  document.querySelector("[data-list-button]").addEventListener("click", () => {
+    const fragment = document.createDocumentFragment();
+  
+    for (const { author, id, image, title } of matches.slice(
+      page * BOOKS_PER_PAGE,
+      (page + 1) * BOOKS_PER_PAGE
+    )) {
+      const element = document.createElement("button");
+      element.classList = "preview";
+      element.setAttribute("data-preview", id);
+  
+      element.innerHTML = `
+              <img
+                  class="preview__image"
+                  src="${image}"
+              />
+              
+              <div class="preview__info">
+                  <h3 class="preview__title">${title}</h3>
+                  <div class="preview__author">${authors[author]}</div>
+              </div>
+          `;
+  
+      fragment.appendChild(element);
+    }
+  
+    document.querySelector("[data-list-items]").appendChild(fragment);
+    page += 1;
+  });
+}
+
+
+// data list items
+export function singleActiveBook(event, authors, books) {
+  const pathArray = Array.from(event.path || event.composedPath());
+  let active = null;
+
+  for (const node of pathArray) {
+    if (active) break;
+
+    if (node?.dataset?.preview) {
+      let result = null;
+
+      for (const singleBook of books) {
+        if (result) break;
+        if (singleBook.id === node?.dataset?.preview) result = singleBook;
+      }
+
+      active = result;
+    }
+  }
+
+
+  if (active) {
+      document.querySelector("[data-list-active]").open = true;
+      document.querySelector("[data-list-blur]").src = active.image;
+      document.querySelector("[data-list-image]").src = active.image;
+      document.querySelector("[data-list-title]").innerText = active.title;
+      document.querySelector("[data-list-subtitle]").innerText = `${
+        authors[active.author]
+      } (${new Date(active.published).getFullYear()})`;
+      document.querySelector("[data-list-description]").innerText =
+        active.description;
+    }
+  };
+
+
+
---
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_DWA09/scripts.js
@@ -0,0 +1,158 @@
+import { books, authors, genres, BOOKS_PER_PAGE } from "./modules/data.js";
+import { 
+  createBookGenerator, 
+  createGenreOptionsFragment,
+  createAuthorOptionsFragment, 
+  setTheme, 
+
+  handleSettingsFormSubmit, 
+  processSearchForm, 
+  handlePreviews, 
+  singleActiveBook,
+} from "./modules/helper.js";
+
+import { 
+  handleSearchOverlays, handleSettingsOverlays, handleHeaderSearchOverlays, handleHeaderSettingsOverlays, handleListOverlays } from "./modules/handlers.js";
+
+let page = 1;
+let matches = books;
+
+// generate starting books
+const bookGenerator = createBookGenerator();
+const startingBooks = bookGenerator.getStartingBooks(matches, BOOKS_PER_PAGE, authors);
+document.querySelector('[data-list-items]').appendChild(startingBooks);
+
+/**
+ * Loads more books onto the page based on the given parameters.
+ *
+ * @param {number} page - The current page number.
+ * @param {Array} matches - An array of book matches.
+ * @param {Array} books - An array of all books.
+ * @param {number} BOOKS_PER_PAGE - The maximum number of books per page.
+ */
+function loadMoreBooks(page, matches, books, BOOKS_PER_PAGE) {
+  document.querySelector("[data-list-button]").innerText = `Show more (${
+    books.length - BOOKS_PER_PAGE 
+  })`;
+  document.querySelector("[data-list-button]").disabled =
+    matches.length - page * BOOKS_PER_PAGE > 0;
+  
+  document.querySelector("[data-list-button]").innerHTML = `
+      <span>Show more</span>
+      <span class="list__remaining"> (${
+        matches.length - page * BOOKS_PER_PAGE > 0
+          ? matches.length - page * BOOKS_PER_PAGE
+          : 0
+      })</span>
+  `;
+}
+
+
+// generate genre options
+const genreOptions = createGenreOptions(genres);
+
+const genreOptionsFragment = createGenreOptionsFragment(genreOptions);
+
+const selectElement = document.querySelector("[data-search-genres]");
+selectElement.appendChild(genreOptionsFragment);
+
+/**
+ * Creates an array of genre options based on the given genres object.
+ *
+ * @param {Object} genres - An object containing genre IDs as keys and genre names as values.
+ * @return {Array} An array of genre options, each containing a value and text property.
+ */
+function createGenreOptions(genres) {
+  const options = [];
+
+  options.push({
+    value: "any",
+    text: "All Genres",
+  })
+
+  for (const [id, name] of Object.entries(genres)) {
+    options.push({
+      value: id,
+      text: name,
+    });
+  }
+  return options;
+} 
+
+
+// generate author options
+const authorOptions = generateAuthorOptions(authors);
+const authorOptionsFragment = createAuthorOptionsFragment(authorOptions);
+document.querySelector("[data-search-authors]").appendChild(authorOptionsFragment);
+
+/**
+ * Creates author options based on an array of authors and returns an array of option objects.
+ * @param {Object} authors - An object with author information.
+ * @return {Array} An array of option objects.
+ */
+function generateAuthorOptions(authors) {
+  const options = [];
+
+  options.push({
+    value: "any",
+    text: "All Authors",
+  });
+
+  for (const [id, name] of Object.entries(authors)) {
+    options.push({
+      value: id,
+      text: name,
+    });
+  }
+  return options;
+}
+
+
+/**
+ * Sets the theme of the user's interface based on their preferred color scheme. 
+ *
+ * @return {void} This function does not return a value.
+ */
+
+function setThemeUsingColourScheme() {
+  if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    setTheme("night");
+  } else {
+    setTheme("day");
+  }
+}
+setThemeUsingColourScheme();
+
+// loading more books
+loadMoreBooks(page, matches, books, BOOKS_PER_PAGE);
+
+
+// overlay handlers
+document.querySelector("[data-search-cancel]").addEventListener("click", handleSearchOverlays);
+document.querySelector("[data-settings-cancel]").addEventListener("click", handleSettingsOverlays);
+document.querySelector("[data-header-search]").addEventListener("click", handleHeaderSearchOverlays);
+document.querySelector("[data-header-settings]").addEventListener("click", handleHeaderSettingsOverlays);
+document.querySelector("[data-list-close]").addEventListener("click", handleListOverlays);
+
+// settings handler
+document
+  .querySelector("[data-settings-form]")
+  .addEventListener("submit", handleSettingsFormSubmit);
+
+
+// search handler
+processSearchForm(authors, books, BOOKS_PER_PAGE, page, matches);
+
+// preview handler
+handlePreviews(authors, books, BOOKS_PER_PAGE, page, matches);
+
+// active book/list handler
+function handleActiveBooks(authors, books) {
+  document
+  .querySelector("[data-list-items]")
+  .addEventListener("click", (event) => {
+    singleActiveBook(event, authors, books);
+  });
+}
+handleActiveBooks(authors, books);
+
