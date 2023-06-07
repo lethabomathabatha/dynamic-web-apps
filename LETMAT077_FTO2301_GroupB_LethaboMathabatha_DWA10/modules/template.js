f5316b0bb3e35bc32c8d9fb0e639e058a7e67bc5
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-06-07 21:11:41 +0200

    edited components

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_DWA10/modules/template.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_DWA10/modules/template.js
@@ -45,19 +45,149 @@ template.innerHTML = /* html */ `
       height: 12rem
     }
 
-    @media (min-width: 600px) {
+    /* mobile */
+    @media (max-width: 424px) {
       .counter__button::part(base) {
-        height: 10rem;
-        width: 45rem;
+        height: 4rem;
+        width: 8rem;
         display: flex;
         align-items: center;
         margin: 1rem;
-        font-size: 4rem;
+        font-size: 2.6rem;
         transition: transform 0.3s;
         transform: translateY(0);
         border-radius: 0.6rem;
       }
-    
+
+      .header__title {
+        font-size: 2rem;
+        font-weight: 100px;
+        letter-spacing: 5px;
+      }
+
+      .counter__value {
+        height: 12rem;
+        font-size: 5rem;
+      }
+
+      .controls {
+        display: grid;
+        margin-bottom: 1rem;
+        padding-top: 2.5rem;
+        border-top: 0.1px solid white; 
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 1.5rem;
+      }
+
+      .footer {
+        font-size: 0.7rem;
+      }
+    }
+
+    /* tablet */
+    @media (max-width: 768px ) {
+      .counter__button::part(base) {
+        height: 6rem;
+        width: 14.5rem;
+        align-items: center;
+        margin: 1rem;
+        font-size: 2.5rem;
+       
+      }
+
+      .header__title {
+        font-size: 4rem;
+        letter-spacing: 3px;
+      }
+
+      .counter__value {
+        height: 15rem;
+        font-size: 6rem;
+      }
+
+      .controls {
+        display: grid;
+        margin-bottom: 1rem;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-gap: 1.2rem;
+      }
+
+      .footer {
+        border-top: 0.1px solid var(--color-light-grey);
+        font-size: 0.9rem;
+      }
+      
+    }
+
+    /* laptop */
+    @media (max-width: 1024px ) {
+      .counter__button::part(base) {
+        height: 7rem;
+        width: 27rem;
+        align-items: center;
+        margin: 1rem;
+        font-size: 2.5rem;
+       
+      }
+
+      .header__title {
+        font-size: 4rem;
+        letter-spacing: 3px;
+        margin-top: 1rem;
+      }
+
+      .counter__value {
+        height: 15rem;
+        font-size: 9rem;
+      }
+
+      .controls {
+        display: grid;
+        margin-bottom: 1.3rem;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-gap: 1.8rem;
+      }
+
+      .footer {
+        border-top: 0.1px solid var(--color-light-grey);
+        font-size: 0.9rem;
+      }
+
+      /* laptop large */
+    @media (max-width: 1440px ) {
+      .counter__button::part(base) {
+        height: 7rem;
+        width: 10rem;
+        align-items: center;
+        margin: 1rem;
+        font-size: 2.5rem;
+       
+      }
+
+      .header__title {
+        font-size: 4rem;
+        letter-spacing: 3px;
+        margin-top: 1rem;
+      }
+
+      .counter__value {
+        height: 15rem;
+        font-size: 9rem;
+      }
+
+      .controls {
+        display: grid;
+        margin-bottom: 1.3rem;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-gap: 2.5rem;
+      }
+
+      .footer {
+        border-top: 0.1px solid var(--color-light-grey);
+        font-size: 0.9rem;
+      }
+      
+    }
   
   </style>
 `;
