b0612016d2bc795afe79e228407ba60953776fdb
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-06-12 10:27:42 +0200

    removed commented out code

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_DWA09/modules/helper.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_DWA09/modules/helper.js
@@ -6,38 +6,6 @@
  * @param {object} authors - An object containing the authors of the books.
  * @return {DocumentFragment} A document fragment containing a list of buttons representing the starting books.
  */
-export function createBookGenerator() {
-  const getStartingBooks = (matches, BOOKS_PER_PAGE, authors) => {
-    const starting = document.createDocumentFragment();
-
-    for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-      const element = document.createElement('button');
-      element.classList = 'preview';
-      element.setAttribute('data-preview', id);
-
-      element.innerHTML = `
-          <img
-              class="preview__image"
-              src="${image}"
-          />
-          
-          <div class="preview__info">
-              <h3 class="preview__title">${title}</h3>
-              <div class="preview__author">${authors[author]}</div>
-          </div>
-      `;
-
-      starting.appendChild(element);
-    }
-
-    return starting;
-  };
-
-  return {
-    getStartingBooks,
-  };
-}
-
 
 
 /**
