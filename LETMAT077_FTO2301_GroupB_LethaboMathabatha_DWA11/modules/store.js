d26633acb8dc9d395cdd644344b35dbd8ccff7be
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-06-18 14:47:17 +0200

    edited state store

--- LETMAT077_FTO2301_GroupB_LethaboMathabatha_DWA11/modules/store.js
+++ LETMAT077_FTO2301_GroupB_LethaboMathabatha_DWA11/modules/store.js
@@ -1,31 +1,32 @@
-// creates the redux store
-
 export const createCountStore = (reducer, initialState) => {
     let state = initialState;
     const countListeners = [];
-
+  
     const getState = () => state;
-
+  
     const dispatch = (action) => {
-        state = reducer(state, action);
-        countListeners.forEach((listener) => listener());
+      const prevState = state; // Store previous state
+      state = reducer(state, action);
+      if (state !== prevState) { // Check if state has changed
+        countListeners.forEach((listener) => listener(state));
+      }
     };
-
-
-const subscribe = (listener) => {
-    countListeners.push(listener);
-
-    return () => {
+  
+    const subscribe = (listener) => {
+      countListeners.push(listener);
+  
+      return () => {
         const index = countListeners.indexOf(listener);
         if (index !== -1) {
-            countListeners.splice(index, 1);
+          countListeners.splice(index, 1);
         }
-    }
-}
-
+      };
+    };
+  
     return {
-        getState,
-        dispatch,
-        subscribe,
-    }
-};
+      getState,
+      dispatch,
+      subscribe,
+    };
+  };
+  
